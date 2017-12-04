287571d0def3d9b18606447a6fb45c7de1fca778
Daniel Cousens <github@dcousens.com>
2017-12-04 22:26:40 +1100

    print each time

--- go.js
+++ go.js
@@ -1,10 +1,10 @@
 let loadLevel = require('./game')
-let level = loadLevel('level1.txt')
+let level = loadLevel('level2.txt')
 let bunny = level.bunny
 
-function goDown () { bunny.move(0, 1) }
-function goLeft () { bunny.move(-1, 0) }
-function goRight () { bunny.move(1, 0) }
-function goUp () { bunny.move(0, -1) }
+function goDown () { bunny.move(0, 1); level.print() }
+function goLeft () { bunny.move(-1, 0); level.print() }
+function goRight () { bunny.move(1, 0); level.print()}
+function goUp () { bunny.move(0, -1); level.print() }
 
 level.print()
